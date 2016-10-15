61aa3e0bba2227146548609c377e862c024ad81e
siranui <siranui87@gmail.com>
2016-10-15 20:51:29 +0900

    repair

--- vimplug.sh
+++ vimplug.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [! -e '~/.vim/plugged/vim-plug']; then
+if [ ! -e ~/.vim/plugged/vim-plug ]; then
    echo 'install vim-plug...'
    mkdir -p ~/.vim/plugged/vim-plug
    git clone https://github.com/junegunn/vim-plug.git ~/.vim/plugged/vim-plug/autoload
+else
+   :
 fi
 
 # vim -c " q! " #-e -s -V1
