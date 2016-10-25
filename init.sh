bff3e528eebdc320e8425fa163eec9e7ed5ccb5a
siranui <siranui87@gmail.com>
2016-10-26 00:53:02 +0900

    repair

--- init.sh
+++ init.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
 
+set -e
 TPM_DIR="$HOME/.tmux/plugins/tpm"
 
+has() {
+   type "$1" > /dev/null 2>&1
+}
+
+# git の存在確認
+if has "git"; then
+   :
+else
+   echo "Install git..."
+   apt install git -y
+fi
+
+# deploy
+for f in .??*
+do
+    [ "$f" = ".git" ] && continue
+
+    ln -sf ~/dotfiles/$f ~/$f
+    echo "$f"
+done
+
+# tpm
 if [ ! -d ${TPM_DIR} ]; then
    echo "Download tpm..."
    git clone https://github.com/tmux-plugins/tpm "${TPM_DIR}"
 fi
+
+# vim-plug
+if [ ! -e ~/.vim/plugged/vim-plug ]; then
+   echo 'install vim-plug...'
+   mkdir -p ~/.vim/plugged/vim-plug
+   git clone https://github.com/junegunn/vim-plug.git ~/.vim/plugged/vim-plug/autoload
+   # Install plugins
+   vim -c "try | PlugInstall | q! | finally | q! | endtry"
+else
+   :
+fi
+
